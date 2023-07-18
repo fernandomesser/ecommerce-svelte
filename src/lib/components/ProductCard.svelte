ebc59123a9d0912d35bb2b724ec6b5fc78c15c25
Fernando Messer <fernandomesser88@gmail.com>
2023-07-17 21:35:24 -0600

    Design

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -5,14 +5,14 @@
      export let product;
 </script>
 
-<div class="border rounded px-8 py-10 space-y-6">
-    <h3 class="font-bold text-xl">{product.name}</h3>
-    <p class="text-gray-600 text-sm">{product.description}</p>
-    <h4 class="font-bold text-4xl">${product.price}</h4>
+<div class="border-black bg-neutral-200 border rounded px-8 py-10 space-y-6 shadow">
+    <h3 class="sm: text-2xl font-bold text-gray-900 sm:text-3xl">{product.name}</h3>
+    <p class="mt-8 flow-root sm:mt-12">{product.description}</p>
     <div>
-        <img src={product.imageUrl} class="w-full" />
+        <img src={product.imageUrl} class="w-full rounded-lg" />
     </div>
+    <h4 class="text-3xl font-bold">${product.price}</h4>
     <div>
-        <a href={product.paymentUrl} class="w-full font-semibold tracking-wider text-lg block text-center px-10 py-5 bg-orange-600 hover:bg-orange-500 text-orange-100">Buy Now</a>
+        <a href={product.paymentUrl} class="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-16 py-6 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">Buy Now</a>
     </div>
 </div>
