3a45c51b232e656d48181e0861517bf5993c31e7
Fernando Messer <fernandomesser88@gmail.com>
2023-07-17 18:53:46 -0600

    Store

---
+++ src/routes/stripe/+page.server.js
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+import { STRIPE_SECRET_KEY } from '$env/static/private'
+
+
+export async function load() {
+    const stripe = new Stripe( STRIPE_SECRET_KEY );
+    const products = await stripe.products.list({
+        limit: 3,
+    });
+}
